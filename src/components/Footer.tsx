e30b7d454e1726d93ec66c0cdd73fc5f9e097013
FREDRICK ODONDI <fredrickodondi95@gmail.com>
2025-06-24 16:46:06 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
                   aria-label={social.label}
                   className="w-10 h-10 glass bg-gray-600 rounded-lg flex items-center justify-center hover:bg-white/20 transition-all duration-300 border border-black"
                 >
-                  <social.icon className="w-5 h-5" />
+                  <social.icon className="w-5 h-5" text-black/>
                 </a>
               ))}
             </div>
