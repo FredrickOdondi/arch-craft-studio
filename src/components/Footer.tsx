bbab1939ccf9ba51ad6870080593be8cf7f09635
FREDRICK ODONDI <fredrickodondi95@gmail.com>
2025-06-24 16:28:01 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
 
 const Footer = () => {
   const socialLinks = [
-    { icon: Threads, href: '#', label: 'Facebook' },
+    { icon: Facebook, href: '#', label: 'Facebook' },
     { icon: Instagram, href: 'https://www.instagram.com/architect_victor_n?igsh=MXNteGFzM2NucWhtaQ==', label: 'Instagram' },
     { icon: Linkedin, href: '#', label: 'LinkedIn' },
     { icon: Twitter, href: '#', label: 'Twitter' }
